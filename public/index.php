52ae390e1554c433c309f8b8e806c08896728ccd
TarekSherif <ENG.TAREK.SHERIF@gmail.com>
2017-04-09 12:29:11 +0200

    TarekSherif Routes

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ require __DIR__ . '/../src/dependencies.php';
 require __DIR__ . '/../src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . '/../TarekSherif/routes.php';
 
 // Run app
 $app->run();
